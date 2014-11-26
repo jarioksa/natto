ae33b1a6d88e10beeff1d7ab90abe2a496aaa4cb
Jari Oksanen <jari.oksanen@oulu.fi>
2014-11-26 16:22:49 +0200

    occpmap queries all observations within projected map
    Now a wrapper that (1) sets a projected map, (2) finds a long/lat
    polygon of the projected map, (3) queries species occurrences
    within that polygon, (4) redraws the map.

--- R/occpmap.R
+++ R/occpmap.R
@@ -8,8 +8,9 @@
 #'
 #' @param CRS Projection used in \code{\link{projectedmap}}.
 #'
-#' @param geometry As defined in \code{\link[spocc]{occ}}, typically
-#' corner points of the longitude-latitude polygon.
+#' @param extent A four element numeric vector of east min, east max,
+#' north min, north max, or other structure accepted by
+#' \code{\link{projectedmap}}.
 #'
 #' @param limit Maximum number of occurrences returned as defined in
 #' \code{\link[spocc]{occ}}.
@@ -18,24 +19,35 @@
 #' latitude and longitude (e.g., \code{pruning = 0.1} keeps only cases
 #' that are not duplicated with 0.1 degree attitude).
 #'
+#' @param clip (logical) Clip map to the occurrences of species
+#' instead of using the original \code{extent}.
+#'
 #' @param \dots Arguments passed to \code{\link[spocc]{occ}} and
 #' \code{\link{projectedmap}}
 #'
 #' @importFrom spocc occ occ2df
 #' 
 #' @export
-`occpmap` <- function(query, CRS = "+proj=longlat", geometry = NULL,
-                      limit = 1000, pruning, ...)
+`occpmap` <- function(query, CRS = "+proj=longlat", extent,
+                      limit = 1000, pruning, clip = TRUE, ...)
 {
-    ## get occurrence data
-    occs <- occ(query, geometry = geometry, limit = limit, ...)
+    ## Get map for the occurrence data
+    map <- projectedmap(extent, CRS = CRS, ...)
+    ## polygon corresponding to the map borders
+    geom <- projectedrect2wkt(map)
+    ## get occurrence data for the map area
+    occs <- occ(query, geometry = geom, limit = limit, ...)
     odf <- occ2df(occs)
     if (!missing(pruning)) {
         dup <- round(odf[,2:3]/pruning)
         odf <- odf[!dup,, drop=FALSE]
     }
     ## projected map
-    map <- projectedmap(odf[,2:3], CRS = CRS, ...)
+    map2 <- projectedmap(odf[,2:3], CRS = CRS, ...)
+    if (clip) {
+        map <- map2
+        attr(map, "input") <- attr(map2, "input")
+    }
     plot(map, ...)
     points(attr(map, "input"), ...)
     invisible(map)
