5c8990298067949c4ca4ca9f40168a31ddde069c
Jari Oksanen <jari.oksanen@oulu.fi>
2015-01-29 19:33:28 +0200

    mean rank shifts gained argument to handle missing species
    Earlier we regarded zero-abundance species as missing, and
    did not rank them nor use in comparing rank shifts. This is
    still the default but an option was added to rank them last.

--- R/mrankdist.R
+++ R/mrankdist.R
@@ -7,6 +7,12 @@
 #'
 #' @param x The input data.
 #'
+#' @param missing If \code{TRUE}, missing species (zero abundance)
+#' will not be used in comparing rank shifts, but rank shifts are only
+#' compared for species that occur in both compared SUs. If
+#' \code{FALSE}, the missing species are given tied rank as last
+#' species.
+#'
 #' @references Collins, S.L, Suding, K.N., Cleland, E.E., Batty, M.,
 #' Pennings, S.C., Gross, K.L., Grace, J.B., Gough, L., Fargione,
 #' J.E. & Clark, C.M. (2008). Rank clocks and plant community
@@ -14,12 +20,16 @@
 #' 
 #' @export
 `mrankdist` <-
-    function(x)
+    function(x, missing = TRUE)
 {
-    ## I assume that rank shifts are only recorded for species present
-    ## in both compared sites: make missing species NA
-    x[x==0] <- NA
-    x <- t(apply(-x, 1, rank, na.last = "keep"))
+    ## If missing=TRUE, rank shifts are only recorded for species
+    ## present in both compared sites: make missing species NA
+    if (missing) {
+        x[x==0] <- NA
+        x <- t(apply(-x, 1, rank, na.last = "keep"))
+    } else {
+        x <- t(apply(-x, 1, rank, na.last = TRUE))
+    }
     d <- dist(x, method = "manhattan")
     ## Manhattan in basic dist() is divided by J/S where J is the
     ## number of species occurring in both sites (when missing species
