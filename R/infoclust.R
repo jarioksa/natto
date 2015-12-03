c7bf52d7500afb0a45d1e1064bdd47d6c6fe4946
Jari Oksanen <jari.oksanen@oulu.fi>
2015-12-03 19:33:04 +0200

    document & tweak function body (comments, return class)

--- R/infoclust.R
+++ R/infoclust.R
@@ -1,22 +1,52 @@
 #' Information Clusterings of Ecological Communities
 #'
-#' Function performs a cluster analysis based on information analysis
-#' as defined by Lambert et al. (1966). The current implementation is
-#' based on Legendre & Legendre (2012).
+#' Function performs hierarchical clustering of binary ecological
+#' communities based on information analysis as defined by Williams et
+#' al. (1966) and Lance & Williams (1966).
 #'
 #' @param x Community data
-#' @param delta Use increase in information (Delta I) instead of the
-#' value of information shown in the tree when merging clusters as
-#' recommended by Lambert et al. (1966, see Legendre & Legendre 2012
-#' for details). If \code{TRUE}, some \code{\link{hclust}} may not
-#' work correctly with the result.
+#' @param delta Use increase in information (\eqn{\Delta I}) instead of the
+#' value of information (\eqn{I}) when merging clusters as
+#' recommended by Williams et al. (1966) and Lance & Williams (1966).
+#'
+#' @details Function performs information analysis of binary
+#' ecological communities (Williams et al. 1966, Lance & Williams
+#' 1966). The current implementation is based on Legendre & Legendre
+#' (2012).
+#'
+#' The information \eqn{I} of a collection of \eqn{N} sampling units
+#' with \eqn{S} species is defined as
+#'   \deqn{I = S N \log N - \sum_i^S a_i \log a_i + (N-a_i) \log (N-a_i)}{I = S*N*log(N) - sum(a*log(a) + (N-a)*log(N-a))}
+#' where \eqn{a} is the frequency count of each species in the
+#' collection. The method works by merging either the units that give
+#' the lowest increase (\eqn{\Delta I} when \code{delta = TRUE}), or
+#' the units that are most homogeneous (lowest \eqn{I} when
+#' \code{delta = FALSE}). After merging sampling units or clusters,
+#' the community data matrix is updated by actually merging the data
+#' units and re-evaluating their information distance to all other
+#' units. The information content of all non-merged clusters is \eqn{I
+#' = 0}, and for clusters of several sampling units the constant
+#' species (completely absent or always present) do not contribute to
+#' the information. The largest increase in information is made by
+#' species with 0.5 relative frequency, so that the analysis tries to
+#' build clusters where species is either always present or always
+#' absent. This often gives easily interpretable clusters.
 #'
 #' @references Williams, W.T., Lambert, J.M. & Lance,
 #' G.N. (1966). Multivariate methods in plant ecology. V. Similarity
 #' analyses and information-analysis. \emph{J. Ecol.} 54, 427--445.
 #'
+#' Lance, G.N. & Williams, W.T. (1966). Computer programs for
+#' hierarchical polythetic classification (\dQuote{similarity
+#' analyses}). \emph{Comp. J.} 9, 60--64.
+#'
 #' Legendre, P. & Legendre, L. (2012). \emph{Numerical Ecology.} 3rd
 #' English Ed., Elsevier.
+#'
+#' @return Function returns an object of class \code{"infoclust"} that
+#' inherits from \code{\link{hclust}}. It uses all \code{"hclust"}
+#' methods, but some may fail or work in unexpected ways because the
+#' analysis is not based on dissimilarities but on binary data matrix.
 #'
 #' @importFrom vegan designdist
 #' 
@@ -39,9 +69,9 @@
     w <- rep(1, N)
     for (lev in 1:(N-1)) {
         ## pick minimum distance
-        if (delta)
+        if (delta) # use increase Delta I
             g <- which.min(dis - outer(adj, adj, "+"))
-        else
+        else       # use the level of heterogeneity I
             g <- which.min(dis)
         g1 <- row(dis)[g]
         g2 <- col(dis)[g]
@@ -84,7 +114,7 @@
                 dist.method = "information",
                 call = match.call(),
                 method = "infoclust")
-    class(out) <- c("hclust", "infoclust")
+    class(out) <- c("infoclust", "hclust")
     out
 }
 
