7f073834fa903b0703ac73f49301b81661c17e70
Jari Oksanen <jari.oksanen@oulu.fi>
2018-09-05 12:35:03 +0300

    rearrange for maintainability

--- R/canneddist.R
+++ R/canneddist.R
@@ -25,27 +25,30 @@
     function(x, method)
 {
     index <- list(
-        ## first proof-of-the-concept
+        ## (A+B-2*J)/(A+B) with various names (and people rave on these)
         "sorensen" = list(method = "(A+B-2*J)/(A+B)", terms = "binary"),
         "bray" =   list(method = "(A+B-2*J)/(A+B)", terms = "minimum"),
         "whittaker" =  list(method = "(A+B-2*J)/(A+B)", terms = "binary"),
-        "ochiai" = list(method = "1-J/sqrt(A*B)", terms = "binary"),
-        "cosine" = list(method = "1-J/sqrt(A*B)", terms = "quadratic"),
+        "steinhaus" = list(method = "(A+B-2*J)/(A+B)", terms = "minimum"),
+        "similarityratio" = list(method = "(A+B-2*J)/(A+B)", terms = "quadratic"),
+        ## Another popular one
+        "jaccard" = list(method = "(A+B-2*J)/(A+B-J)", terms = "binary"),
+        "ruzicka" = list(method = "(A+B-2*J)/(A+B-J)", terms = "minimum"),
         ## Legendre & Legendre: metric distances
         "euclidean" = list(method = "sqrt(A+B-2*J)", terms = "quadratic"),
         "chord" = list(method = "sqrt(2*(1-J/sqrt(A*B)))", terms = "quadratic"),
         "geodesic" = list(method = "acos(J/sqrt(A*B))", terms = "quadratic"),
         "manhattan" = list(method = "A+B-2*J", terms = "minimum"),
-        ## L & L: binary similarity indices as distances 1-S in terms A,B,J
-        "jaccard" = list(method = "(A+B-2*J)/(A+B-J)", terms = "binary"),
+        "ochiai" = list(method = "1-J/sqrt(A*B)", terms = "binary"),
+        "cosine" = list(method = "1-J/sqrt(A*B)", terms = "quadratic"),
+        ## Legendre & Legendre have some oddities, here as dissimilarities
         "triplejaccard" = list(method = "(A+B-2*J)/(A+B+J)", terms = "binary"),
         "sokalsneath" = list(method = "2*(A+B-2*J)/(2*A+2*B-3*J)", terms="binary"),
         "russellrao" = list(method = "1-J/P", terms = "binary"),
-        "bin.kulczynski14" = list(method = "(A+B-3*J)/(A+B-2*J)", terms = "binary"),
-        "bin.kulczynski15" = list(method = "1-J/2/A-J/2/B", terms="binary"),
-        ## L & L: quantitative similarity indices as distances 1-S
-        "steinhaus" = list(method = "(A+B-2*J)/(A+B)", terms = "minimum"),
-        "kulczynski" = list(method =  "1-J/2/A-J/2/B", terms = "minimum"))
+        "alt.kulczynski" = list(method = "(A+B-3*J)/(A+B-2*J)", terms = "binary"),
+        ## Kulczynskis
+        "kulczynski" = list(method =  "1-J/2/A-J/2/B", terms = "minimum"),
+        "bin.kulczynski" = list(method = "1-J/2/A-J/2/B", terms="binary"))
 
     ind <- match.arg(method, names(index))
     z <- index[[ind]]
