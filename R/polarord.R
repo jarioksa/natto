b38e4e06a40300671060ce103ed32d2e620f5dbf
Jari Oksanen <jari.oksanen@oulu.fi>
2018-09-14 10:23:06 +0300

    roxygen documentation

--- R/polarord.R
+++ R/polarord.R
@@ -2,6 +2,83 @@
 ### McCune & Jim Grace (2002), Analysis of Ecological Communities,
 ### Chapter 17
 
+#' Polar (Bray-Curtis) Ordination
+#'
+#' Polar or Bray-Curtis ordination is a historic ordination method
+#' that could be performed without computers with simple hand
+#' calculations (Bray & Curtis 1957). Ordination axis is found by
+#' selecting two extreme points and projecting all points between
+#' these end points. The current function follows Beals (1984) in
+#' selecting the endpoints, projection of points on axis, and defining
+#' the residual distances for later axes.
+
+#' @param d Dissimilarities or distances: a \code{\link{dist}} object.
+#' @param k Number of dimensions.
+#'
+#' @details
+#'
+#' The implementation follows McCune & Grace (2002, Chapter 17). The
+#' endpoints are found using the variance-regression method of Beals
+#' (1984). The first endpoint has the highest variance of distances to
+#' other points. This guarantees that the point is at the margin of
+#' the multivariate cloud but is not an outlier, since outliers have
+#' long distances to all points and hence low variance of
+#' distances. The second endpoint has the lowest (most negative)
+#' regression coefficient between distances from the first and second
+#' point to all other points. This selects a point at the margin of
+#' the main cloud of points, opposite to first endpoint. All points
+#' are projected on the axis between the endpoints using Euclidean
+#' geometry, and this gives the scores on a polar ordination
+#' axis. Then the effect of the  axis are removed by calculating
+#' residual distances using Euclidean geometry. Ecological indices are
+#' usually semimetric, and negative residual distances can emerge, but
+#' these are taken as zero in the current function.
+#'
+#' Polar ordination is a historical method that is little used today,
+#' but several authors claim that it is a powerful method (see McCune
+#' & Grace 2002). Although the basic operations can be easily
+#' performed by hand or graphically, the later developments of
+#' endpoint selection require more extensive calculations. With modern
+#' numerical utilities, the polar ordination is not faster than metric
+#' multidimensional scaling (\code{\link{cmdscale}},
+#' \code{\link[vegan]{wcmdscale}}).
+#'
+#' It is possible to use predefined endpoints instead of automatic
+#' selection. This can be useful for confirmatory analysis (McCune &
+#' Grace 2002). However, this is not (yet) implemented in this
+#' function (but contributions are welcome).
+#'
+#' @return
+#'
+#' Currently the function returns only a matrix of ordination scores,
+#' but this will change.
+#'
+#' @references
+#'
+#' Beals, E. W. (1984) Bray-Curtis ordination: an effective strategy
+#' for analysis of ecological multivariate data. \emph{Advances in
+#' Ecological Research} 14, 1--55.
+#'
+#' Bray, J. R. & Curtis, J. T. (1957) An ordination of the upland
+#' forest communities in southern Wisconsin. \emph{Ecological
+#' Monographs} 27, 325--349.
+#'
+#' McCune, B. & Grace, J. B. (2002) \emph{Analysis of Ecological
+#' Communities.} MjM Software Design.
+#'
+#' @examples
+#'
+#' data(spurn)
+#' ## We still need vegan, but work for self-standing case
+#' if(require(vegan)) {
+#' dis <- vegdist(spurn)
+#' ord <- polarord(dis)
+#' ordiplot(ord, type = "t")
+#' }
+#'
+#' @importFrom stats var cov dist
+#'
+#' @export
 `polarord` <-
     function(d, k=2)
 {
@@ -14,12 +91,14 @@
         m <- as.matrix(d)
         ## Find the first endpoint (variance method)
         p1 <- which.max(apply(m, 2, var))
-        ## Find the second endpoint (regression method)
+        ## Find the second endpoint (regression method: regression
+        ## coefficient was originally suggested, but covariance gives
+        ## the same results, but faster)
         p2 <- which.min(cov(m[p1,],m))
         ## Project all points between these endpoints
         axes[,dim] <- (m[p1,p2]^2  + m[p1,]^2 - m[p2,]^2)/2/m[p1,p2]
         ## Find the residual dissimilarities, with a guard against
-        ## negative residuals in semimetric indices
+        ## negative residuals in semimetric indices & oblique axes
         d <- sqrt(pmax(d^2 - dist(axes[,dim])^2,0))
     }
     axes
