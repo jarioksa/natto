2ae48095c027267ea9f67f4e96a84f66713314b0
Jari Oksanen <jari.oksanen@oulu.fi>
2018-11-12 17:52:01 +0200

    new recycling rules

--- R/ortho.R
+++ R/ortho.R
@@ -11,6 +11,6 @@
     function(x0, x)
 {
     for(i in 1:ncol(x0))
-        x <- x - crossprod(x0[,i],x) / crossprod(x0[,i],x0[,i]) * x0[,i]
+        x <- x - drop(crossprod(x0[,i],x)/crossprod(x0[,i],x0[,i])) * x0[,i]
     x
 }
