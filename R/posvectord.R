0a5f69d909abaf83507a5e0dff30cec2ed531bab
Jari Oksanen <jari.oksanen@oulu.fi>
2018-11-14 20:19:53 +0200

    document Position & Species Vector Ordination
    For some reason tools::document gives a
    Warning: @title : needs a value, but the processed man/posvectord.Rd
    has title and renders without warnings. The warning on missing
    @title appeared when I edited quite different parts not related
    to the @title.

--- R/posvectord.R
+++ R/posvectord.R
@@ -1,15 +1,52 @@
-#' Position Vector Ordination
+#' Position Vector and Species Vector Ordination
 #'
-#' Position Vector Ordination (PVO) is a simple educational method of
-#' that resembles Principal Component Analysis (PCA). The method picks
-#' the species or variable that explains largest proportion of
-#' variance and uses the centred values of this variable as the
-#' axis. Then it orthogonalizes all species or variables to that
-#' selected axis and repeats the selection process. In PCA a linear
-#' combination of species or variables is found to minimize the
-#' residual variance, but in PVO only one species or variable is
-#' used. The axes are named by the species or variables, and the axis
-#' scores are the centred (residual) observed values.
+#' Position Vector Ordination (PVO) finds ordination axes that go
+#' through sample points in species space and maximize the projections
+#' of other points onto them. These are similar to Principal
+#' Components. Species Vector Ordination (SVO) finds a set of species
+#' that maximize the projections of other species on species
+#' axes. This also can be similar to Principal Components and can be
+#' used to select a subset of species that cover most of the variance
+#' in the data.
+#'
+#' Position Vector Ordination (PVO, function \code{posvectord}) is a
+#' simple educational method that resembles Principal Component
+#' Analysis, PCA (Orlóci 1966, 1973a). Function \code{posvectord}
+#' positions vectors from the data centroid (origin) to sample points
+#' in species space and evaluates the projections of other sample
+#' vectors on these positioned axes, and selects the one with highest
+#' total projection as the ordination axis. Then the effects of that
+#' selected axis are removed from the covariance matrix, zeroing the
+#' row and column of selected sampling unit, and the process is
+#' repeated. Principal Components maximize this projection, but PVO
+#' axes can be close to the Principal Component, in particular in the
+#' large data sets with many observations. The method was proposed as
+#' a computationally light approximation to PCA suitable for the
+#' computers of 1960s (Orlóci 1966). Now it can only be regarded as
+#' historically interesting, and also as an educational tool in
+#' introducing PCA.
+#'
+#' Species Vector Ordination (SVO; function \code{spvectord}) is
+#' similar, but it picks the species vector that maximizes the species
+#' projected onto that vector (Orlóci 1973b).  The method picks the
+#' species or variable that explains largest proportion of variance
+#' and uses the centred values of this variable as the axis. Then it
+#' orthogonalizes all species or variables to that selected axis and
+#' repeats the selection process. In PCA a linear combination of
+#' species or variables is found to minimize the residual variance,
+#' but in SVO only one species or variable is used. The axes are named
+#' by the species or variables, and the axis scores are the centred
+#' (residual) observed values. The resulting plot has orthogonalized
+#' set of species as axes. SVO can be similar to the PCA solution, in
+#' particular when some few species contribute most of the the total
+#' variance. The method only has educational use in explaining PCA in
+#' species space. Orlóci (1973b) suggesed SVO as a method of selecting
+#' a subset of species or variables that contributes most of the
+#' variance in the data. The current \code{spvectord} function can
+#' also be used for this purpose, although it is mainly geared for
+#' ordination. \CRANpkg{dave} package has function
+#' \code{\link[dave]{orank}} specifically written for variable or
+#' species selection using the same algorithm.
 #'
 #' @encoding UTF-8
 #'
@@ -19,12 +56,19 @@
 #' @return The function returns an object of class \code{"posvector"}
 #'  with following elements:
 #'  \itemize{
-#'     \item \code{points}: The ordination scores. These are named by
-#'       the species (variable) the axes is based on, the numerical
-#'       scores are the centred (residual) values of observed data.
+#'    \item \code{points}: The ordination scores. In SVO, these are
+#'     named by the species (variable) the axes is based on, the
+#'     numerical scores are the centred (residual) values of
+#'     observed data. In PVO, they are called as PVO, and they are
+#'     scaled similarly as in PCA and can reconstitute
+#'     (a low-rank approximation of) covariances.
 #'    \item \code{totvar}: The total variance in the input data.
 #'    \item \code{eig}: Eigenvalues of axes.
-#'  }
+#'}
+#'
+#' @seealso \code{\link{polarord}} (Polar Ordination) is a similar
+#'     educational tool to approximate Principal Coordinates Analysis
+#'     (PCoA).
 #'
 #' @references
 #'
