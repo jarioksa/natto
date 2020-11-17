735ee46fe3d77ff13cb632b6ced972d07c38de9d
Jari Oksanen <jhoksane@gmail.com>
2020-11-17 12:28:45 +0200

    add function for residual species association in constrained ordination
    This is inpsired by HMSC package and its computeSpeciesAssociaations
    function. In HMSC, random effects are estimated via laten variables
    for SUs and species, and species associations are correlation-like
    standardized crossproduct of species loadings. Vague advice of related
    practice have been expressed in constrained ordination suggesting that
    inspecting residual ordination, but this is rarely done (and there are
    not many tools available, apart from fitting with new environmental
    variables). Bayesian HMSC uses factor model with only a limited number
    of latent variables, but constrained ordination finds full rank solution.
    If we use all unconstrained axes, the species are often nearly uncorrelated,
    but they can show clear patterns in some first unconstrained axes. The
    default is to use vegan::bstick() to find the number of "significant"
    residual axes, but users can set the number of axes used.

---
+++ R/resassoc.R
@@ -0,0 +1,27 @@
+#' @importFrom stats cov2cor
+#' @importFrom vegan bstick
+#'
+#' @export
+`resassoc.rda` <-
+    function(x, rank, ...)
+{
+    if (!inherits(x, "cca"))
+        stop("input must be a constrained ordination object from vegan")
+    if (is.null(x$CA$eig))
+        stop("object 'x' does not have residual ordination")
+    ## use bstick to get the rank if not given
+    if (missing(rank)) {
+        bs <- bstick(m$CA$rank, m$CA$tot.chi)
+        rank <- min(which(bs > m$CA$eig)) - 1
+    }
+    ## Residual associations can be estimated only when rank > 1, else
+    ## return identity matrix
+    if (rank < 2) {
+        r <- diag(NROW(m$CA$v))
+        dimnames(r) <- list(rownames(m$CA$v), rownames(m$CA$v))
+    } else {
+        r <- cov2cor(tcrossprod(m$CA$v[, seq_len(rank)]))
+    }
+    attr(r, "rank") <- rank
+    r
+}
