f009bddf2c11afd0abf62268d71e9cb283b126ee
Jari Oksanen <jhoksane@gmail.com>
2022-02-01 14:52:39 +0200

    sets are faster than membership matrix (by a factor of 4)

--- R/bayesjaccard.R
+++ R/bayesjaccard.R
@@ -1,23 +1,17 @@
 clsupport <-
-    function (x, n=1000, method="average", usesets = FALSE, plot = TRUE, ...)
+    function (x, n=1000, method="average", plot = TRUE, ...)
 {
     h0 <- hclust(designdist(x, "(b+c+1)/(a+b+c+2)", term="bin", abcd=TRUE),
                  method)
-    m0 <- if (usesets) clsets(h0) else clmembers(h0)
+    m0 <- clsets(h0)
     supp <- s <- numeric(nrow(h0$merge)-1)
     for(i in seq_len(n)) {
         d <- designdist(x, "rbeta(length(a), b+c+1, a+1)",
                         term="bin", abcd=TRUE)
         h <- hclust(d, method)
-        if (usesets) {
-            m <- clsets(h)
-            for (i in seq_along(supp))
-                s[i] <- any(sapply(m, function(x) all(identical(m0[[i]], x))))
-        } else {
-            m <- clmembers(h)
-            s <- sapply(seq_len(ncol(m)), function(i)
-                any(apply(sweep(m, 1, m0[,i], "=="), 2, all)))
-        }
+        m <- clsets(h)
+        for (i in seq_along(supp))
+            s[i] <- any(sapply(m, function(x) all(identical(m0[[i]], x))))
         supp <- supp + s
     }
     if (plot) {
@@ -25,23 +19,6 @@ clsupport <-
         ordilabel(h0, "internal", labels=supp)
     }
     supp
-}
-
-
-clmembers <-
-    function (hclus)
-{
-    m <- hclus$merge
-    memb <- matrix(FALSE, nrow(m)+1, nrow(m)-1)
-    for (j in seq_len(nrow(m) - 1)) {
-        for (k in 1:2) {
-            if(m[j,k] < 0)
-                memb[-m[j,k], j] <- TRUE
-            else
-                memb[,j] <- memb[,j] | memb[,m[j,k]]
-        }
-    }
-    memb
 }
 
 clsets <-
