0a6a8c8a0d90000fcf424fc867319f7f03467d2f
Jari Oksanen <jhoksane@gmail.com>
2022-05-05 16:49:34 +0300

    document and export canneddist

--- R/canneddist.R
+++ R/canneddist.R
@@ -4,21 +4,70 @@
 #' called by their vernacular names. The function is based on
 #' \code{\link[vegan]{designdist}} (\CRANpkg{vegan} package).
 #'
+#' @details
+#'
+#' Function wraps popular dissimilarity indices for
+#' \code{\link[vegan]{designdist}} allowing these to be called by
+#' their popular names. It can have synonymous names for one
+#' dissimilarity index. Use argument \code{help=TRUE} to see the
+#' current selection of indices and their definitions.
+#'
+#' The function uses the main notation of
+#' \code{\link[vegan]{designdist}} where terms are based on sums and
+#' paired minima or sum of squares and crossproducts of pairs of
+#' sampling units. For vectors \code{x} and \code{y} the
+#' \code{"quadratic"} terms are \code{J = sum(x*y)}, \code{A =
+#' sum(x^2)}, \code{B = sum(y^2)}, and \code{"minimum"} terms are
+#' \code{J = sum(pmin(x,y))}, \code{A = sum(x)} and \code{B = sum(y)},
+#' and \code{"binary"} terms are either of these after transforming
+#' data into binary form (number of shared species \code{J}, and
+#' number of species for each row, \code{A, B}.). Number of columns
+#' (species) is notated as \code{P}, and the number of sampling units
+#' is \code{N}.
+#'
+#' There is a huge number of indices, and the current selection is far
+#' from comprehensive (but it can easily expanded). See References for
+#' sources. Many sources use different notation, but they were changed
+#' to the notation described above. For instance, in popular (but
+#' strange) 2x2 contingency table notation for binary data \code{a =
+#' J}, \code{b = A-J}, \code{c = B-J}, \code{d = P-A-B+J}. Some of
+#' formulae may be surprising, but they are mathematically equivalent
+#' to traditional ones. I challenge you to inspect Euclidean distance,
+#' and once you see how it is derived, try Chord distance.
+#'
+#' @encoding UTF-8
+#' @references
+#'
+#' \enc{Hubálek}{Hubalek}, Z. (1982). Coefficients of association and
+#' similarity, based on binary (presence-absence) data: an
+#' evaluation. \emph{Biological Review} 57, 669--689.
+#'
+#' Legendre, P. & Legendre, L. (2012). \emph{Numerical Ecology.} 3rd
+#' English Ed., Elsevier.
+#'
 #' @param x Input data.
 #' @param method Vernacular name for a dissimilarity index.
-#' @param help List available indices instead of calculating dissmilarities.
+#' @param help List available indices and their definitions instead of
+#'     calculating dissimilarities.
 #'
 #' @return Function returns a \code{\link{dist}} object of dissimilarities.
 #'
 #' @author Jari Oksanen
 #'
-#' @seealso \code{\link[vegan]{designdist}}.
+#' @seealso
+#'
+#' Function is a wrapper to
+#' \code{\link[vegan]{designdist}}. \CRANpkg{vegan} function
+#' \code{\link[vegan]{betadiver}} is a similar collection of canned
+#' indices for beta diversity, and many of these are well-known
+#' dissimilarity indices.
 #'
 #' @examples
 #' data(spurn)
 #' ## Ochiai dissimilarity
 #' canneddist(spurn, "ochiai")
 #'
+#' @importFrom stats phyper
 #' @importFrom vegan designdist
 #'
 #' @export
