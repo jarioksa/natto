0480a630a7716c5ef608a0183ce71596a9381eba
Jari Oksanen <jhoksane@gmail.com>
2023-04-24 19:33:24 +0300

    standardize data so that standard tools give Rao diversity & distance
    The files were written in 2016, and even considered these for
    vegan. Let's have a look at them in natto first.

---
+++ R/raostand.R
@@ -0,0 +1,32 @@
+### standardize data matrix 'x' with distances 'd' so Simpson
+### diversity will be Rao's quadratic entroy and Euclidean distances
+### will be (transformed) Rao dissimilarity: x <- x %*% (1-d)^1/2
+`raostand`<-
+    function(x, d)
+{
+    TOL <- sqrt(.Machine$double.eps)
+    x <- as.matrix(x)
+    dn <- attr(x, "dimnames")
+    ## distances d to similarity matrix
+    if (max(d) > 1)
+        d <- d/max(d)
+    d <- as.matrix(d)
+    d <- 1 - d
+    ## check that diagonals are 1
+    if (any(abs(diag(d) - 1) > TOL))
+        stop("'d' is not a valid dissimilarity object")
+    ## eigen decomposition
+    e <- eigen(d)
+    ## all eigenvalues should be positive
+    if (any(e$values < -TOL))
+        stop("dissimilarities 'd' do not define Euclidean transformation")
+    k <- e$values > TOL
+    ## matrix squareroot
+    vec <- e$vectors[, k, drop=FALSE]
+    ev <- e$values[k]
+    d <- vec %*% (sqrt(ev) * t(vec))
+    ## transform
+    x <- x %*% d
+    attr(x, "dimnames") <- dn
+    x
+}
