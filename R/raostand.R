6b7444d28d4c8c140fb198b8fdf67331748f3afd
Jari Oksanen <jhoksane@gmail.com>
2023-04-25 15:53:30 +0300

    arguments to make raostand compatible with qrao & raodist

--- R/raostand.R
+++ R/raostand.R
@@ -6,19 +6,36 @@
 #' Function standardizes data so that it gives Rao phylogenetic
 #' diversity and Euclidean distance give Rao phylogenetic distance.
 #'
+#' @importFrom vegan decostand
+#'
 #' @param x Community data.
-#' @param d Phylogenetic distances.
+#' @param d Phylogenetic distances or other dissimilarities among
+#'     species.
+#' @param propx Standardize row of \code{x} to unit total. This will
+#'     give standardization that is compatible with \code{qrao} and
+#'     \code{distrao}.
+#' @param dmax Scale dissimilarities by \code{dmax} (if \code{dmax >
+#'     max(d)}) or truncate dissimilarities at \code{dmax} (if
+#'     \code{dmax < max(d)}).
 #'
 #' @export
 `raostand`<-
-    function(x, d)
+    function(x, d, propx = TRUE, dmax)
 {
     TOL <- sqrt(.Machine$double.eps)
     x <- as.matrix(x)
+    if (propx)
+        x <- decostand(x, "tot")
     dn <- attr(x, "dimnames")
     ## distances d to similarity matrix
-    if (max(d) > 1)
-        d <- d/max(d)
+    d <- as.dist(d)
+    if (!missing(dmax)) {
+        d <- d/dmax
+        if (max(d, na.rm = na.rm) > 1)
+            d[d > 1] <- 1
+    }
+    else if (max(d, na.rm = na.rm) > 1)
+        d <- d/max(d, na.rm = na.rm)
     d <- as.matrix(d)
     d <- 1 - d
     ## check that diagonals are 1
