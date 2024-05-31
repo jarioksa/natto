042e72cfbf12a62ed68227879c213932613e36b2
Jari Oksanen <jhoksane@gmail.com>
2024-05-31 10:41:02 +0300

    name to taxondist, format, document & export

--- R/taxondist.R
+++ R/taxondist.R
@@ -1,22 +1,33 @@
-#' Return Gammaplus and Thetaplus Elements of Clarke's Taxonomic Community Dissimilarity
+### Clarke's taxonomic distance was suggested for vegan in
+### https://github.com/vegandevs/vegan/issues/430. This function is
+### based on a function of a response to that request.
+
+#' Return Gammaplus and Thetaplus Elements of Clarke's Taxonomic
+#' Community Dissimilarity
 #'
-#' @param x Community data; will be treated as binary presence/absence matrix.
-#' @param taxdist Taxonomic, phylogenetic or other dissimilarity matrix among species (columns of x).
+#' @param x Community data; will be treated as binary presence/absence
+#'     matrix.
+#' @param taxdist Taxonomic, phylogenetic or other dissimilarity
+#'     matrix among species (columns of x).
 #'
-#' @return list of Clarke's Gammaplus and Thetaplus elements as distance structures.
-vegtaxdist <- 
-    function (x, taxdist) 
+#' @return list of Clarke's Gammaplus and Thetaplus elements as
+#'     distance structures.
+#' @export
+`taxondist` <-
+    function (x, taxdist)
 {
     x <- as.matrix(x)
     x <- ifelse(x > 0, 1, 0)
     taxdist <- as.matrix(taxdist)
-    if (NCOL(taxdist) != NCOL(x)) stop("Number of columns do not match in 'x' and 'taxdist'")
+    if (NCOL(taxdist) != NCOL(x))
+        stop("Number of columns do not match in 'x' and 'taxdist'")
     N <- NROW(x)
     Gammaplus <- matrix(0, N, N)
     Thetaplus <- matrix(0, N, N)
     for(j in 1:(N-1)) {
         for(i in (j+1):N) {
-           crosstd <- (outer(x[i,], x[j,]) * taxdist)[x[i,] > 0, x[j,] > 0, drop = FALSE]
+            crosstd <- (outer(x[i,], x[j,]) * taxdist)[x[i,] > 0, x[j,] > 0,
+                                                       drop = FALSE]
            min1 <- apply(crosstd, 1, min)
            min2 <- apply(crosstd, 2, min)
            Gammaplus[i,j] <- mean(c(min1, min2))
