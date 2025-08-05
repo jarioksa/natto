fff9682724b43f531402a2bd36469bb448d09905
Jari Oksanen <jhoksane@gmail.com>
2025-08-05 12:58:10 +0300

    implement rough basic version of constrained NMDS
    1. NMDS of constrained dissimilarities
    2. vegan::NMDSaddpoints to add same points based on community
       dissimilarities.

---
+++ R/cNMDS.R
@@ -0,0 +1,30 @@
+#' Constrained Nonmetric Multidimensional Scaling
+#'
+#' @param formula Model formula where the left-hand side must be
+#'     dissimilarities, and right-hand side the constraints.
+#' @param data Data frame to find the terms on the right-hand side of
+#'     the formula.
+#' @param k Number of dimensions in NMDS.
+#'
+#' @importFrom stats cmdscale
+#' @importFrom vegan monoMDS, MDSaddpoints
+#'
+#' @export
+`cNMDS` <-
+    function(formula, data, k = 2)
+{
+    ## step 1: constrained dissimilarities
+    cdis <- distconstrain(formula, data)
+    ## step 2: NMDS of constrained dissimilarities
+    m0 <- cmdscale(cdis, k = k)
+    sol <- monoMDS(cdis, m0, k = k)
+    ## step 3: Constrained ommunity ordination
+    dis <- eval(formula[[2]])
+    m2 <- MDSaddpoints(sol, as.matrix(dis))
+    ## construct result object
+    sol$constraints <- sol$points
+    sol$points <- m2$points
+    sol$stress <- sol$stress + m2$deltastress
+    class(sol) <- c("cNMDS", class(sol))
+    sol
+}
