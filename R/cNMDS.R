a6cb1162bed4f12e48abd689f20c5a53b8fd83c4
Jari Oksanen <jhoksane@gmail.com>
2025-08-05 17:22:09 +0300

    cNMDS: works more smoothly with internal functions
    - zeroes possible negative constrained dissimilarities and accept
      argument 'add' for additive constant to avoid neg squared
      dissimilarities
    - fixes arg name in rank from tie to tie.method
    - result object now monoMDS compatible and can prints cleanly
      (but this needs vegan 2.7-2).

--- R/cNMDS.R
+++ R/cNMDS.R
@@ -19,28 +19,43 @@
 #' @param data Data frame to find the terms on the right-hand side of
 #'     the formula.
 #' @param k Number of dimensions in NMDS.
+#' @param add Type of additive constant used to avoid negative squared
+#'     distances in \code{\link{distconstrain}}. Either
+#'     \code{"lingoes"} or \code{"cailliez"} or \code{FALSE}.
 #'
 #' @importFrom stats cmdscale
 #' @importFrom vegan monoMDS, MDSaddpoints
 #'
 #' @export
 `cNMDS` <-
-    function(formula, data, k = 2)
+    function(formula, data, k = 2, add = FALSE)
 {
     ## step 1: constrained dissimilarities
-    cdis <- distconstrain(formula, data)
+    cdis <- distconstrain(formula, data, add = add, squared = TRUE)
+    if (any(cdis < 0)) {
+        message("negative squared dissimilarities changed to 0")
+        cdis[cdis < 0] <- 0
+    }
     ## step 2: NMDS of constrained dissimilarities
-    m0 <- cmdscale(cdis, k = k)
-    cdis[] <- rank(cdis, ties = "min")
+    m0 <- cmdscale(sqrt(cdis), k = k)
+    cdis[] <- rank(cdis, ties.method = "min")
     sol <- monoMDS(cdis, m0, k = k)
-    ## step 3: Constrained ommunity ordination
+    ## step 3: Constrained community ordination
     dis <- eval(formula[[2]])
-    dis[] <- rank(dis, ties = "min")
+    dis[] <- rank(dis, ties.method = "min")
     m2 <- MDSaddpoints(sol, as.matrix(dis))
-    ## construct result object
+    ## construct result object: inherits from vegan::monoMDS
     sol$constraints <- sol$points
     sol$points <- m2$points
     sol$stress <- sol$stress + m2$deltastress
+    attr(sol$points, "pc") <- FALSE
+    sol$call <- match.call()
+    sol$model <- "Constrained non-metric"
+    sol$distmethod <- attr(dis, "method")
+    sol$distcall <- NULL
+    sol$iters <- m2$iters
+    sol$icause <- m2$cause
+    sol$iscal <- FALSE
     class(sol) <- c("cNMDS", class(sol))
     sol
 }
