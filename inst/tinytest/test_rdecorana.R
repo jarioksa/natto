d1982e1d3caf58f2f20eb0dc4c7ae636850ad6b1
Jari Oksanen <jhoksane@gmail.com>
2026-04-15 13:42:56 +0300

    add verification test of gradrescale & expect silenty rdecorana work

--- inst/tinytest/test_rdecorana.R
+++ inst/tinytest/test_rdecorana.R
@@ -7,21 +7,29 @@ require(vegan, quietly=TRUE)
 data(dune)
 ## 1. default call implying rescaling with iresc=4
 m0 <- vegan::decorana(dune)
-m <- rdecorana(dune)
+expect_silent(m <- rdecorana(dune))
 expect_equivalent(m0$evals.decorana, m$evals, tol=1e-7)
 expect_equivalent(abs(diag(cor(m0$rproj, m$rproj))), c(1,1,1,1), tol=1e-5)
 expect_equivalent(abs(diag(cor(m0$cproj, m$cproj))), c(1,1,1,1), tol=1e-5)
 
 ## 2. no rescaling, only detrending
 m0 <- vegan::decorana(dune, iresc = 0)
-m <- rdecorana(dune, iresc = 0)
+expect_silent(m <- rdecorana(dune, iresc = 0))
 expect_equivalent(m0$evals.decorana, m$evals, tol=1e-7)
 expect_equivalent(abs(diag(cor(m0$rproj, m$rproj))), c(1,1,1,1), tol=1e-5)
 expect_equivalent(abs(diag(cor(m0$cproj, m$cproj))), c(1,1,1,1), tol=1e-5)
 
 ## 3. Orthogonal CA
 m0 <- vegan::decorana(dune, ira=1)
-m <- rdecorana(dune, ira=1)
+expect_silent(m <- rdecorana(dune, ira=1))
 expect_equivalent(m0$evals, m$evals, tol=1e-8)
 expect_equivalent(abs(diag(cor(m0$rproj, m$rproj))), c(1,1,1,1), tol=1e-7)
 expect_equivalent(abs(diag(cor(m0$cproj, m$cproj))), c(1,1,1,1), tol=1e-7)
+
+## gradrescale: should be equal to decorana(t(x), iresc=1) column score 1
+m0 <- vegan::decorana(dune, iresc = 0)
+u <- m0$rproj[,1]
+expect_silent(uscaled <- gradrescale(u, dune))
+## one rescaling of transposed data to get rescaled "species" score
+mtrans <- vegan::decorana(t(dune), iresc = 1)
+expect_equal(cor(mtrans$cproj[,1], uscaled), 1)
