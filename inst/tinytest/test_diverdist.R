2bf0877e8c4931351179d0f1be67dbcd0dc10f0b
Jari Oksanen <jhoksane@gmail.com>
2026-05-02 15:00:05 +0300

    update and polish diverclust and diverdist
    - equalize has now three choices as character strings: "renyi" and
      "no" correspond to old TRUE and FALSE, and new "total" equalized
      sampling units to unit total (proportions)
    - combined documentation of diverdist to diverclust
    - add test on equalization

--- inst/tinytest/test_diverdist.R
+++ inst/tinytest/test_diverdist.R
@@ -1,9 +1,13 @@
 ### diverdist & diverclust
 require(vegan, quietly=TRUE)
-data(spurn)
+data(spurn, package="natto")
 ## renyi=, hill=TRUE defines increase in number of species
 expect_equivalent(diverdist(spurn, renyi=0, hill=TRUE),
                   designdist(spurn, "(A+B-2*J)/2", "binary"))
 ## no real way to verify that the results are correct
-expect_silent(cl <- diverclust(spurn))
+expect_silent(cl <- diverclust(spurn, renyi=Inf, equalize="renyi"))
+expect_silent(cl0 <- diverclust(decostand(spurn, "max", MARGIN=1),
+                                renyi=Inf, equalize="no"))
 expect_inherits(cl, "hclust")
+expect_equal(cl$merge, cl0$merge)
+expect_equal(cl$height, cl0$height)
