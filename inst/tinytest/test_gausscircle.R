a38e5f2b4291606c40908b807e4af07bd36ad2f7
Jari Oksanen <jhoksane@gmail.com>
2026-06-03 16:45:38 +0300

    add verification tests for gausscircle

---
+++ inst/tinytest/test_gausscircle.R
@@ -0,0 +1,25 @@
+## Verification test for gausscircle:
+## Generate error-free 2D Gaussian response, and estimate its
+## generating species parameters with gausscircle.
+
+nobs <- 30
+nsp <- 6
+p <- matrix(0, nsp, 4)
+colnames(p) <- c("xopt", "yopt", "tol", "top")
+p[,1:2] <- runif(2*nsp, -2, 2)
+p[,3] <- rnorm(nsp, 1, 0.2)
+p[,4] <- rexp(nsp, 1/5)
+x <- matrix(runif(2 * nobs, -2, 2), nrow = nobs)
+y <- matrix(NA, nrow = nobs, ncol = nsp)
+for(i in 1:nsp)
+    y[,i] <- p[i,4] * exp(-rowSums(sweep(x, 2, p[i,1:2])^2)/2/p[i,3]^2)
+## Re-find input response paramaters p
+expect_silent(res <- gausscircle(x, y))
+expect_equal(res[, 1:4], p)
+## test for unit-tolerance responses
+for (i in 1:nsp)
+    y[,i] <- p[i,4] * exp(-rowSums(sweep(x, 2, p[i,1:2])^2)/2)
+expect_silent(res <- gausscircle(x, y, unit = TRUE))
+expect_equal(res[, c(1,2,4)], p[, c(1,2,4)])
+expect_equal(gausscircle(x, y, unit = TRUE), gausscircle(x, y))
+
