6465a56cf7d8269a2673c68079dd47bbc1870b06
Jari Oksanen <jhoksane@gmail.com>
2026-06-10 13:56:30 +0300

    modernize code to changes in the cca-object since 2018 (vegan 2.5-1)

--- R/as.prcomp.rda.R
+++ R/as.prcomp.rda.R
@@ -1,7 +1,11 @@
-#' Cast vegan::rda Result to base::prcomp
+#' Cast vegan::rda Result to stats::prcomp
 #'
 #' Function casts a result object of unconstrained
-#' \code{\link[vegan]{rda}} to a \code{\link{prcomp}} result object.
+#' \code{\link[vegan]{rda}} or \code{\link[vegan]{pca}} to a
+#' \code{\link{prcomp}} result object, but without the last zero
+#' eigenvalue and rubbish eigenvectors of \code{prcomp}. The
+#' translation is best evaluated by looking at the code of the
+#' function.
 #'
 #' @param x An unconstrained \code{\link[vegan]{rda}} result object.
 #'
@@ -28,13 +32,13 @@
     structure(
         list(sdev = sqrt(x$CA$eig),
              rotation = x$CA$v,
-             center = attr(x$CA$Xbar, "scaled:center"),
-             scale = if(!is.null(scl <- attr(x$CA$Xbar, "scaled:scale")))
+             center = attr(x$Ybar, "scaled:center"),
+             scale = if(!is.null(scl <- attr(x$Ybar, "scaled:scale")))
                          scl
                      else
                          FALSE,
              x = scores(x, display = "sites", scaling = 1,
-             choices = seq_len(x$CA$rank),
+                        choices = seq_len(x$CA$rank),
              const = sqrt(x$tot.chi * (nrow(x$CA$u)-1)))),
         class = "prcomp")
 }
